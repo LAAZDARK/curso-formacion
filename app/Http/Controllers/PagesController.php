04b8736f51bdeecbdf65f4911f5652e67a58a8c7
Luis Alberto Amador Zamorano <luis.alberto@webdecero.com>
2021-11-06 22:40:36 -0600

    Admin lts

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\Models\User;
+
+// use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+// use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Validator;
+
+
+
+
+class PagesController extends Controller
+{
+
+    public function viewHome()
+    {
+
+        return view('pages.index');
+    }
+
+
+
+}
